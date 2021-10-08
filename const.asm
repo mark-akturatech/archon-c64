572feea1084c6c7743b15d0b52bc0b52c509ef8e
Mark Beljaars <mark@akturatech.com>
2021-10-08 23:12:24 +1000

    init

---
+++ const.asm
@@ -0,0 +1,10 @@
+//---------------------------------------------------------------------------------------------------------------------
+// Application constants
+//---------------------------------------------------------------------------------------------------------------------
+
+.const VICMEM   = $8000             // start of video memory
+.const CHRMEM1  = VICMEM + $0000    // start of lowert lower character set memory
+.const CHRMEM2  = VICMEM + $0800    // start of upper case character set memory
+.const SCNMEM   = VICMEM + $0400    // start of screen memory
+.const SPTMEM   = SCNMEM + $07f8    // start of sprite pointer memory
+.const GRPMEM   = VICMEM + $1000    // start of graphics memory
---
+++ io.asm
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------------------------------------------------
+// C64 I/O constants
+// Inspired by C64-Mark
+// https://github.com/C64-Mark/Attack-of-the-Mutant-Camels/blob/master/Original/IO.asm
+//---------------------------------------------------------------------------------------------------------------------
+
+// Processor
+.const R6510    = $01   // Processort port
+.const FREEZP   = $FB;  // Four Free Bytes of Zero Page for User Programs
+.const CINV     = $0314 // Vector to IRQ Interrupt Routine
+.const CBINV    = $0316 // Vector: BRK Instruction Interrupt
+
+// VIC
+.const  SP0X    = $D000 // Sprite 0 Horizontal Position
+.const  MSIGX   = $D010 // Most Significant Bits of Sprites 0-7 Horizontal Position
+.const  SCROLY  = $D011 // Vertical Fine Scrolling and Control Register
+.const  RASTER  = $D012 // Read Current Raster Scan Line/Write Line to Compare for Raster IRQ
+.const  SPENA   = $D015 // Sprite display enable
+.const  SCROLX  = $D016 // VIC Control Register 2
+.const  VMCSB   = $D018 // VIC Memory Control Register
+.const  VICIRQ  = $D019 // VIC Interrupt Flag Register
+.const  IRQMASK = $D01A // IRQ Mask Register
+.const  SPMC    = $D01C // Sprite multi-colour select
+.const  XXPAND  = $D01D // Sprite X horizontal expand
+
+// CIA2
+.const  CI2PRA  = $DD00 // Port A access
+.const  C2DDRA  = $DD02 // Port A direction
